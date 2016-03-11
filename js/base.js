0287f39809acb9a6e682d1044779acdd1a09d1b8
drdynscript <drdynscript@gmail.com>
2016-03-11 14:23:52 +0100

    Update gdm.gent

--- js/base.js
+++ js/base.js
@@ -1,21 +1,68 @@
-(function() {
+(function($) {
     
-	var toggleOpen = document.querySelector('.nav-toggle--open');
+    var offCanvasType = $('body').data('offcanvasType');
+    var offCanvasDirection = $('body').data('offcanvasDirection');
+    $('.offcanvas__sidebar').css(offCanvasDirection, $('.offcanvas__sidebar').width() * -1 + 'px');
+    
+    var offcanvasNav = $('.offcanvas__nav');
+    if(offcanvasNav) {
+		offcanvasNav.on('click', function(ev) {
+			changeStateOffcanvas(true);
+		});
+	}
+    
+    var toggleOpen = $('.offcanvas__toggle--open');
 	if(toggleOpen) {
-		toggleOpen.addEventListener('click', function(ev) {
+		toggleOpen.on('click', function(ev) {
 			ev.preventDefault();
-			document.querySelector('body').classList.toggle('js__nav-is-open');
+            changeStateOffcanvas($('body').hasClass('js__offcanvas-is-open'));
 			return false;
 		});
 	}
 	
-	var toggleClose = document.querySelector('.nav-toggle--close');
+	var toggleClose = $('.offcanvas__toggle--close');
 	if(toggleClose) {
-		toggleClose.addEventListener('click', function(ev) {
+		toggleClose.on('click', function(ev) {
 			ev.preventDefault();
-			document.querySelector('body').classList.remove('js__nav-is-open');
+			changeStateOffcanvas(true);
 			return false;
 		});
 	}
+    
+    var toggleOverlay = $('.offcanvas__content-overlay');
+    if(toggleOverlay) {
+		toggleOverlay.on('click', function(ev) {
+			ev.preventDefault();
+			changeStateOffcanvas(true);
+			return false;
+		});
+	}
+    
+    function changeStateOffcanvas(isOpen) {
+        var cssMarginDirection = 'margin-left';
+        var offCanvasMargin = (isOpen)?0:$('.offcanvas__sidebar').width();
+        
+        switch(offCanvasDirection) {
+            case 'left':default:
+                cssMarginDirection = 'margin-left';break;
+            case 'right':
+                cssMarginDirection = 'margin-right';break;
+        }
+        
+        switch(offCanvasType) {
+            case 'push':
+                $('.offcanvas__content').css(cssMarginDirection, offCanvasMargin);break;
+            default:break;
+        }
+        
+        if(isOpen) {
+            $('body').removeClass('js__offcanvas-is-open');
+            $('.offcanvas__sidebar').css(offCanvasDirection, $('.offcanvas__sidebar').width() * -1 + 'px');
+        } else {
+            $('body').addClass('js__offcanvas-is-open');
+            $('.offcanvas__sidebar').css(offCanvasDirection, 0);
+        }
+    }
 	
-})();
+	
+})(jQuery);
